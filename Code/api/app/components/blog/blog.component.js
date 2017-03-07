8ca05665089cefae5bf89f1f063605061a51334e
npere181 <npere181@fiu.edu>
2017-03-06 19:01:29 -0500

    More features to blog!

--- Code/api/app/components/blog/blog.component.js
+++ Code/api/app/components/blog/blog.component.js
@@ -6,17 +6,28 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
 var core_1 = require("@angular/core");
+var forms_1 = require("@angular/forms");
 var BlogComponent = (function () {
     function BlogComponent() {
+        this.blogForm = new forms_1.FormGroup({
+            title: new forms_1.FormControl(),
+            content: new forms_1.FormControl(),
+            author: new forms_1.FormControl()
+        });
     }
+    BlogComponent.prototype.onSubmit = function () {
+        console.log(this.blogForm.value);
+    };
     return BlogComponent;
 }());
+
 BlogComponent = __decorate([
     core_1.Component({
         moduleId: module.id,
         selector: 'blog-app',
         templateUrl: 'blog.component.html',
-        styleUrls: ['blog.component.css']
+        styleUrls: ['blog.component.css'],
+        styles: ["\n    input-ng-invalid {broder-left: 5px solid red;}\n    input-ng-valid {broder-left: 5px solid green;}"]
     })
 ], BlogComponent);
 exports.BlogComponent = BlogComponent;
--- Code/api/app/components/blog/blog.component.ts
+++ Code/api/app/components/blog/blog.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
     moduleId: module.id,
     selector: 'blog-app',
     templateUrl: 'blog.component.html',
-    styleUrls: ['blog.component.css']
+    styleUrls: ['blog.component.css'],
+    styles: [`
+    input-ng-invalid {broder-left: 5px solid red;}
+    input-ng-valid {broder-left: 5px solid green;}`]
 })
 
 export class BlogComponent {
+ blogForm = new FormGroup({
+        title: new FormControl(),
+        content: new FormControl(),
+         author: new FormControl()
+    });
 
+    onSubmit() {
+        console.log(this.blogForm.value);
+    }
 }
