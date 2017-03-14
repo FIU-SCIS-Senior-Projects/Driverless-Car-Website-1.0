78264eb063827d2f142bf2e4640eae57800f2a51
npere181 <npere181@fiu.edu>
2017-03-14 14:36:21 -0400

    Blog mongodb

--- Code/routes/admin.js
+++ Code/routes/admin.js
@@ -1,18 +1,17 @@
 var express = require('express');
 var router = express.Router();
-
 var mongojs = require('mongojs');
 
-var db = ('mongodb://admin:admin123@ds139959.mlab.com:39959/driverlesscar', ['admin']);
+var db = mongojs('mongodb://admin:admin123@ds139959.mlab.com:39959/driverlesscar', ['admin']);
 
 router.get('/blog/login', function(req, res, next) {
-    res.send('login page');
-    // db.admin.finds(function(err, admin) {
-    //     if (err) {
-    //         res.send();
-    //     }
-    //     res.json(admin);
-    // })
-})
+    //res.send('login page');
+     db.admin.find(function(err, admin) {
+        if (err) {
+            res.send(err);
+        }
+        res.json(admin);
+    });
+});
 
 module.exports = router;
--- Code/routes/blog.js
+++ Code/routes/blog.js
@@ -1,11 +1,42 @@
 var express = require('express');
 var router = express.Router();
+var mongojs =  require('mongojs');
 
+//for cloud connection(mlab) with credentials
+var db = mongojs('mongodb://admin:admin123@ds139959.mlab.com:39959/driverlesscar',['blogs'])
+
+router.get('/blog', function(req,res,next){
+    
+    //res.send('TASK API');
+
+    //gets all json file on the collection
+    db.blogs.find(function(err,blogs){
+        if(err){
+            res.send(err);
+        }
+       // res.json(blogs);
+        res.render('index.html');
+        
+    });
+});
+
+router.post('/blog',function(req,res,next){
+    var task = req.body;
+    if(!task.title|| !(task.body + '')){
+        res.status(400);
+        res.json({
+            "error": "Bad Data"
+        });
+    }else{
+        db.task.save(task,function(err,task){
+            if(err){
+                res.send(err);
+            }
+            res.json(task);
+        });
+    }  
+ });
 
 //set routers
-
-router.get('/blog', function(req, res, next) {
-    res.render('index.html');
-});
 
 module.exports = router;
