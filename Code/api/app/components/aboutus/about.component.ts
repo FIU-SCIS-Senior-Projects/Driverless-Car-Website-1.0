6957ef60b7f0dbb6e791d985e5c4b73284cfb903
npere181 <npere181@fiu.edu>
2017-04-14 14:57:30 -0400

    Added download button to product page and fix image size, finished technology page, fixed homepage div spaces, fixed about us page added new team members and pictures!

--- Code/api/app/components/aboutus/about.component.ts
+++ Code/api/app/components/aboutus/about.component.ts
@@ -10,11 +10,14 @@ import { Component } from '@angular/core';
 export class AboutComponent {
     fullImagePath: string;
     fullImagePath2: string;
-
+    fullImagePath3: string;
+     fullImagePath4: string;
 
     constructor() {
         this.fullImagePath = 'images/aboutus/nathally.png'
-        this.fullImagePath2 = 'images/aboutus/mariana.png'
+        this.fullImagePath2 = 'images/aboutus/mariana.jpg'
+        this.fullImagePath3 = 'images/aboutus/kianoosh.jpg'
+        this.fullImagePath4 = 'images/aboutus/mohsen.jpg'
     }
     ngOnInit() {
 
